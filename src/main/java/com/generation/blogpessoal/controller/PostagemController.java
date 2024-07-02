437fcf546b335f7719a70c788768b22ec5414daa
Julia <jrhuliafy@gmail.com>
2024-07-02 13:24:38 -0300

    Criação do Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,39 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens") //geralmente sempre em letra minuscula
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository postagemRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem>getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+
+
+}
